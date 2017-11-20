c878adebe739ca8f0511027a201c9c5971c8ee16
youku_sm <thebottomofwaterofhali@gmail.com>
2017-11-21 08:46:49 +0900

    first commit

---
+++ src/Main.elm
@@ -0,0 +1,28 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text, program)
+import Messages exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
