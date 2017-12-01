5b84fed0ad4d1e7b56cdf7eaf324e1b7016359de
youku_sm <thebottomofwaterofhali@gmail.com>
2017-12-02 00:13:26 +0900

    コミット漏れ

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var config = {
+    saveUrl: "https://4goxl59jqg.execute-api.ap-northeast-1.amazonaws.com/prod/save",
+    detailUrl: "https://4goxl59jqg.execute-api.ap-northeast-1.amazonaws.com/prod",
+    cloneUrl: "http://localhost/dakatsu/soba/clone",
+    outputUrl: "http://localhost/dakatsu/soba/output"
+};
